e0135a704046ec841eb8f772a2c7f7f76e30e136
lowcodelow <hasanimgecelik@gmail.com>
2025-02-26 03:07:36 +0300

    Create card.js

---
+++ components/ui/card.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Card = ({ children }) => {
+  return (
+    <div className="border rounded-lg shadow-md p-4 bg-white">
+      {children}
+    </div>
+  );
+};
+
+export const CardContent = ({ children }) => {
+  return <div className="p-2">{children}</div>;
+};
